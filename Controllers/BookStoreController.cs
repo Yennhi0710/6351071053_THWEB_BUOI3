65eb3d1db17a590e63c828889d531bf8cdcadaca
Yennhi0710 <131666857+Yennhi0710@users.noreply.github.com>
2024-11-16 00:14:38 +0700

    Add files via upload

---
+++ Controllers/BookStoreController.cs
@@ -0,0 +1,61 @@
+﻿using BookStore.Models;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class BookStoreController : Controller
+    {
+        // GET: BookStore
+
+        protected readonly QuanLyBanSachDataContext data;
+
+        public BookStoreController()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["QLBANSACHConnectionString"].ConnectionString;
+            data = new QuanLyBanSachDataContext(connectionString);
+        }
+        public ActionResult Index(int ? page)
+        {
+            int pageSize = 1;
+
+            int pageNum = (page ?? 1);
+            var sachmoi = Laysachmoi(15);
+            return View(sachmoi.ToPagedList(pageNum, pageSize));
+        }
+        private List<SACH> Laysachmoi(int count)
+        {
+            return data.SACHes.OrderByDescending(a => a.Ngaycapnhat).Take(count).ToList();
+        }
+        public ActionResult Chude()
+        {
+            var chude = from cd in data.CHUDEs select cd;
+            return PartialView(chude);
+        }
+        public ActionResult Nhaxuatban()
+        {
+            var nhaxb = from cd in data.NHAXUATBANs select cd;
+            return PartialView(nhaxb);
+        }
+        public ActionResult SPTheocd(int id)
+        {
+            var sach = from s in data.SACHes where s.MaCD==id select s;
+            return View(sach);
+        }
+        public ActionResult SPTheoNXB(int id)
+        {
+            var sach = from s in data.SACHes where s.MaNXB == id select s;
+            return View(sach);
+        }
+        public ActionResult Details(int id)
+        {
+            var sach = from s in data.SACHes where s.Masach == id select s;
+            return View(sach.Single());
+        }
+    }
+}
